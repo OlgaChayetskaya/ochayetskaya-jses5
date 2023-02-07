79097caa0c07d5e2162c2f21ca086016077b4d83
Dzianis Hetsikau <dzianis.hetsikau@ibm.com>
2023-02-07 19:01:01 +0200

    Initial commit - added required files

---
+++ ext/mountains.js
@@ -0,0 +1,12 @@
+var MOUNTAINS = [
+  {name: "Kilimanjaro", country: "Tanzania"},
+  {name: "Everest", height: 8848, country: "Nepal"},
+  {name: "Mount Fuji", height: 3776, country: "Japan"},
+  {height: 4808, country: "Italy/France"},
+  {name: "Vaalserberg", height: 323, country: "Netherlands"},
+  {name: "Denali", height: 6168, country: "United States"},
+  {name: "Popocatepetl", height: 5465}
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = MOUNTAINS;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import x from './app/Task1/task1';
+
+let t = x();
+console.log (t);
