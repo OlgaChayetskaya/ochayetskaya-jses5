7c691feb79648495b6cc1f8cfd6d8feeb6c41597
OlgaChayetskaya <Ochayetskaya@ibagroup.eu>
2023-02-10 17:38:35 +0100

    Task3 Upd

--- src/app/Task3/task3.js
+++ src/app/Task3/task3.js
@@ -11,14 +11,26 @@
 // Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a
 // way to fix this?
 function isEven(number) {
-  if (number < 0) {
-    return;
-  }
+  number < 0 ? (k = -1) : (k = 1);
+
   if (number === 0) {
     return true;
   } else if (number === 1) {
     return false;
   } else {
-    return isEven(number - 2);
+    return isEven(number - 2 * k);
   }
 }
+
+// or
+
+function isEven1(number) {
+  if (Math.abs(number) === 0) {
+    return true;
+  } else if (Math.abs(number) === 1) {
+    return false;
+  } else {
+    return isEven1(Math.abs(number) - 2 );
+  }
+}
+
