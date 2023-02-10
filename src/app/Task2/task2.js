00eee3226df93f016de919d711e51947a7f984ea
OlgaChayetskaya <Ochayetskaya@ibagroup.eu>
2023-02-10 15:50:40 +0100

    Task2

---
+++ src/app/Task2/task2.js
@@ -0,0 +1,11 @@
+// Minimum
+// The previous chapter introduced the standard function Math.min that returns its
+// smallest argument. We can do that ourselves now. Write a function min that
+// takes two arguments and returns their minimum.
+function customMin(arg1, arg2) {
+  if (arg1 <= arg2) {
+    return arg1;
+  } else {
+    return arg2;
+  }
+};
